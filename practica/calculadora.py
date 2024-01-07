2454f9b13a74801edfb268e3f5caa5e229443ad0
Esteban Vazquez <mestebanve123@gmail.com>
2024-01-07 07:27:22 -0800

    Se realizo el menu de opciones

---
+++ practica/calculadora.py
@@ -0,0 +1,8 @@
+print("Calculadora que suma, resta, multiplica y divide")
+
+print("Menú de opciones.")
+print("1. Suma")
+print("2. Resta")
+print("3. Multiplicación")
+print("4. División")
+opcion = float(input("Ingrese la opción deseada: "))

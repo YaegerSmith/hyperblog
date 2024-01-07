9aa9c4e5c0e495395caef86a6c947f6b85d4e1a1
Esteban Vazquez <mestebanve123@gmail.com>
2024-01-07 07:32:02 -0800

    Se realizo el codigo de la suma, resta, multiplicacion y division

--- practica/calculadora.py
+++ practica/calculadora.py
@@ -6,3 +6,23 @@ print("2. Resta")
 print("3. Multiplicación")
 print("4. División")
 opcion = float(input("Ingrese la opción deseada: "))
+if opcion == 1:
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    print("La suma es: ", num1 + num2)
+elif opcion == 2:
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    print("La resta es: ", num1 - num2)
+elif opcion == 3:
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    print("La multiplicación es: ", num1 * num2)
+    
+elif opcion == 4:
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    print("La división es: ", num1 / num2)
+    
+else:
+    print("Opción no válida")
